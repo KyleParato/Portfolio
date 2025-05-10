14a60498cb98269d89497470f1feafcaecb9c5c4
KyleParato <kyleparato@gmail.com>
2025-05-09 20:37:57 -0700

    Finished Skills Page

--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -8,27 +8,27 @@ export const Navbar = () => {
         <ul className="list-style-none">
         <div className="divide-x-2 divide-solid divide-orange-400">
           <div className="hover:bg-orange-300/50 flex-1 inline-block hover:rounded-l-md">
-            <li className="flex-1 m-4 l font-medium sm:text-lg md:text-xl lg:text-3xl" data-twe-nav-item-ref> 
+            <li className="flex-1 m-4 l font-medium sm:text-base md:text-xl lg:text-3xl" data-twe-nav-item-ref> 
               <a href="/" data-twe-nav-link-ref>About</a>
             </li>
           </div>
           <div className="hover:bg-orange-300/50 flex-1 inline-block">
-            <li className="flex-1 m-4 l font-medium sm:text-lg md:text-xl lg:text-3xl" data-twe-nav-item-ref> 
+            <li className="flex-1 m-4 l font-medium sm:text-base md:text-xl lg:text-3xl" data-twe-nav-item-ref> 
               <a href="/projects" data-twe-nav-link-ref>Projects</a>
             </li>
           </div>
           <div className="hover:bg-orange-300/50 flex-1 inline-block">
-            <li className="flex-1 m-4 l font-medium sm:text-lg md:text-xl lg:text-3xl" data-twe-nav-item-ref>
+            <li className="flex-1 m-4 l font-medium sm:text-base md:text-xl lg:text-3xl" data-twe-nav-item-ref>
               <a href="/skills" data-twe-nav-link-ref>Skills</a>
             </li>
           </div>
           <div className="hover:bg-orange-300/50 flex-1 inline-block">
-            <li className="flex-1 m-4 l font-medium sm:text-lg md:text-xl lg:text-3xl" data-twe-nav-item-ref>
+            <li className="flex-1 m-4 l font-medium sm:text-base md:text-xl lg:text-3xl" data-twe-nav-item-ref>
               <a href="/contact" data-twe-nav-link-ref>Contact</a>
             </li>
           </div>
           <div className="hover:bg-orange-300/50 flex-1 inline-block hover:rounded-r-md">
-            <li className="flex-1 m-4 l font-medium sm:text-lg md:text-xl lg:text-3xl" data-twe-nav-item-ref>
+            <li className="flex-1 m-4 l font-medium sm:text-base md:text-xl lg:text-3xl" data-twe-nav-item-ref>
               <a target="_blank" href={Resume} data-twe-nav-link-ref>Resume</a>
             </li>
           </div>
--- portfolio/src/pages/skills.tsx
+++ portfolio/src/pages/skills.tsx
@@ -1,5 +1,6 @@
 import { title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
+import {Card, CardBody, CardFooter, CardHeader} from "@heroui/card";
 
 export default function DocsPage() {
   return (
@@ -9,8 +10,78 @@ export default function DocsPage() {
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>Skills</h1>
         </div>
-        {/* Gallery */}
-        
+        {/* Services */}
+        <Card>
+          <CardHeader>
+            <h1 className="text-xl font-bold">Services</h1>
+          </CardHeader>
+          <div className="grid md:grid-cols-2 sm:grid-cols-1">
+            <CardBody className="m-5">
+              <p className="font-semibold">Amazon Web Services</p>
+              <img src="/src/img/aws.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">PowerApps</p>
+              <img src="/src/img/microsoft-power-apps.png" class="icon-img"/>
+            </CardBody>
+          </div>
+        </Card>
+        {/* Languages */}
+        <Card>
+          <CardHeader>
+            <h1 className="text-xl font-bold">Languages</h1>
+          </CardHeader>
+          <div className="grid md:grid-cols-4 sm:grid-cols-1">
+            <CardBody className="m-5">
+              <p className="font-semibold">Python3</p>
+              <img src="/src/img/python.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">C++</p>
+              <img src="/src/img/C++.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">JavaScript</p>
+              <img src="/src/img/JavaScript.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">Tailwind</p>
+              <img src="/src/img/tailwind.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">HTML5</p>
+              <img src="/src/img/HTML5.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">Swift</p>
+              <img src="/src/img/swift.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">SQL</p>
+              <img src="/src/img/SQL.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">C#</p>
+              <img src="/src/img/csharp.png" class="icon-img"/>
+            </CardBody>
+          </div>
+        </Card>
+        {/* Programs */}
+        <Card>
+          <CardHeader>
+            <h1 className="text-xl font-bold">Programs</h1>
+          </CardHeader>
+          <div className="grid md:grid-cols-2 sm:grid-cols-1">
+            <CardBody className="m-5">
+              <p className="font-semibold">Wireshark</p>
+              <img src="/src/img/wireshark.png" class="icon-img"/>
+            </CardBody>
+            <CardBody className="m-5">
+              <p className="font-semibold">Unity</p>
+              <img src="/src/img/unity.png" class="icon-img"/>
+            </CardBody>
+          </div>
+        </Card>
       </section>
     </DefaultLayout>
   );
